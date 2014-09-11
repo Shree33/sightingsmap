7a2a1cd0aeabf0a0852d658dee4e88a96d3daaee
Sam Purcell <spurcell93@gmail.com>
2014-09-11 16:47:38 -0400

    Initial commit - data sanitation and maps instantiation.

---
+++ main.js
@@ -0,0 +1,52 @@
+(function(){
+
+    function parseInts(obj) {
+        _.each(obj, function(val, key) {
+            if (!_.isNaN(parseInt(val))) {
+                obj[key] = parseInt(val)
+            }    
+        })
+        return obj;
+    }
+
+    var Bird = Backbone.Model.extend({
+        parse: function(r) {
+            var p = "gsx$";
+            // Get only relevant properties
+            var sanitized = _.pick(r, p+"lat", p+"lng", p+"ll", p+"lr", p+"ul", p+"ur", p+"locationbanded", p+"sightingdate", p+"bandnumber" )
+            // Get values from sub-objects
+            _.each(sanitized, function(val, key) {
+                sanitized[key] = val.$t;
+            })
+            sanitized = _.invert(sanitized)
+            // Strip gsx prefixes
+            _.each(sanitized, function(val, key) {
+                sanitized[key] = val.replace(p, "");
+            })
+            return parseInts(_.invert(sanitized))
+        }
+    });
+
+    var Birds = Backbone.Collection.extend({
+        model: Bird,
+        url: "https://spreadsheets.google.com/feeds/list/17fGFqzDS8uOQKmAXRjCscY_f4l85XjEtTsvlawUYwN0/o39yaik/public/values?alt=json",
+        parse: function(response) {
+            return response.feed.entry
+        }
+    });
+
+
+    var sightings = new Birds()
+    sightings.fetch({
+        parse: true,
+        success: function(coll) {
+            console.log(coll);
+        }
+    });
+
+    new google.maps.Map(document.getElementById("map-canvas"), {
+        center: { lat: 20.7, lng: -156.9601584},
+        zoom: 8
+    })
+
+}).call(this);
