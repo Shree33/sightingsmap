86726f931429e82706073b9d4bf3c5efb2dd6acc
Zabir Islam <zabir.islam@tufts.edu>
2015-11-12 04:13:55 -0500

    Removed BOTH fit to bounds

--- map.js
+++ map.js
@@ -51,7 +51,7 @@ define(["messenger"], function(messenger) {
             else { 
                 messenger.dispatch("reset:timeline")
             }
-            that.fitToBounds();
+            //that.fitToBounds();
         })
 
         messenger.when("show:markers add:sightings", function() {
