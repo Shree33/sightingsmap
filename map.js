b4d36af5a73e26f071c2706fb5140b0e11e72b9f
Zabir Islam <zabir.islam@tufts.edu>
2015-12-03 10:41:08 -0500

    Trying to make new markerclusterer without map

--- map.js
+++ map.js
@@ -44,7 +44,7 @@ define(["messenger"], function(messenger) {
             zoom: 8
         });    
 
-	MarkerCluster = new MarkerClusterer(this.map);
+	MarkerCluster = new MarkerClusterer();
 
         var that = this;
         this.active_sighting_models = new Backbone.Collection();
